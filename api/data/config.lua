6e8df131ae2c317134ea10cb6925b47caabd86cc
09030146152TeaM <37263388+09030146152TeaM@users.noreply.github.com>
2018-03-12 14:50:18 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,14 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  moderation = {
+    data = "/home/TiiGeR/TiiGeR/cli/data/moderation.json"
+  },
+  sudo_users = {
+  }
+}
+return _
+end
